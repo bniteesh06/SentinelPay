98166679c6c619d8fd71ea206c4842ae2496bf32
raga-shree-r <ragashree086@gmail.com>
2026-09-01 09:43:38 +0530

    Created navbar-hero

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // SentinelPay script.js
-// Person A adds the nav toggle code here
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => {
+    const isOpen = navLinks.classList.toggle('is-open');
+    navToggle.setAttribute('aria-expanded', isOpen);
+  });
+}
+
 // Person C adds the stat counter code here
